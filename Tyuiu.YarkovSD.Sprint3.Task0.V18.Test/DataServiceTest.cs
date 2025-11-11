f1c6a501503ba011eea3209d76cfd9a125cf787a
BorneO <savelis1908@gmail.com>
2025-11-11 13:00:45 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.YarkovSD.Sprint3.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.YarkovSD.Sprint4.Task0.V18.Lib
+{
+    public class DataService : ISprint4Task0V18
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int a = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    a *= array[i];
+                }
+            }
+
+            return a;
+        }
+    }
+}
