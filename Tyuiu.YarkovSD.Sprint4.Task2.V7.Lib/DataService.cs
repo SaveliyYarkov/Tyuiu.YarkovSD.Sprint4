17c30df63e41dc4b1dc14066fb35b5b6f1e8bf44
BorneO <savelis1908@gmail.com>
2025-11-11 13:36:59 +0300

    Task2

---
+++ Tyuiu.YarkovSD.Sprint4.Task2.V7.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.YarkovSD.Sprint4.Task2.V7.Lib
+{
+    public class DataService : ISprint4Task2V7
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] % 2 == 0)
+                    sum += array[i];
+            return sum;
+        }
+    }
+}
