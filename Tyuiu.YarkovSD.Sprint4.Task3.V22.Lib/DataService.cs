2ede51a9b341a98db8c056255bcb1ebaaea455da
BorneO <savelis1908@gmail.com>
2025-11-11 14:08:15 +0300

    Task3

---
+++ Tyuiu.YarkovSD.Sprint4.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.YarkovSD.Sprint4.Task3.V22.Lib
+{
+    public class DataService : ISprint4Task3V22
+    {
+        public int Calculate(int[,] array)
+        {
+            int m = -9999;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                    for (int j = 0; j < array.GetLength(0); j++)
+                        if (array[0, j] > m)
+                            m = array[i, j];
+            return m;
+        }
+    }
+}
