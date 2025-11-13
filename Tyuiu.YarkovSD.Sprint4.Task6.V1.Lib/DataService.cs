8f4ced9046c21163f16777698ca50cf290a2afc9
BorneO <savelis1908@gmail.com>
2025-11-13 15:07:54 +0300

    Task6

---
+++ Tyuiu.YarkovSD.Sprint4.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.ExceptionServices;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.YarkovSD.Sprint4.Task6.V1.Lib
+{
+    public class DataService : ISprint4Task6V1
+    {
+        public int Calculate(string[] array)
+        {
+            int a = 0;
+            foreach (string i in array)
+            {
+                if (i.Length > 0);
+                {
+                    a ++;
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint4.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint4.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
