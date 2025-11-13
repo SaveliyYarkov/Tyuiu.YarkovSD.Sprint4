4084cb2975f47fa309963aca33fcf3bbda8e940d
BorneO <savelis1908@gmail.com>
2025-11-13 14:51:42 +0300

    Task4

---
+++ Tyuiu.YarkovSD.Sprint4.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint4.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
